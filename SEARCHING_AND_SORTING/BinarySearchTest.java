1621c947db82f2211cc162c266fa5b326e594a38
Uko Chibuike Malachi <113272653+malachi43@users.noreply.github.com>
2025-08-05 14:41:59 +0100

    modified BinarySearchTest.java file

--- SEARCHING_AND_SORTING/BinarySearchTest.java
+++ SEARCHING_AND_SORTING/BinarySearchTest.java
@@ -9,6 +9,7 @@ public class BinarySearchTest{
         int high = array.length - 1;
         int middle = ((low + high) + 1) /2;
 
+        //Each iteration tests the middle value of the remaining portion of the array 
         while(low <= high){
             if(array[middle] == searchKey) return middle;
             else if(searchKey < array[middle]){
@@ -24,27 +25,27 @@ public class BinarySearchTest{
     public static void main(String[] args) {
         System.out.println();
 
-        int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+        int[] numbers = {13,18,29,36,42,47,56,57,63,68,80,81,82,88,88};
 
         String visualArray = Arrays.toString(numbers);
         System.out.println("Array: " + visualArray);
         System.out.print("Index: ");
         
-        for(int i = 0; i < numbers.length; i++){
-            System.out.printf(" %d ", i);
+        for(int i = 0; i < numbers.length - 1; i++){
+            System.out.printf("  %d ", i);
         }
         System.out.println();
         System.out.println();
 
-        for(int i = 0; i < 11; i++){
-            int searchKey =  i;
+        // for(int i = 0; i < 11; i++){
+            int searchKey =  49;
             int index = binarySearch(numbers, searchKey);
             if(index != -1){
                 System.out.printf("The searchkey %d is found at index %d%n",searchKey, index);
             }else{
                 System.out.printf("The searchkey %d cannot be found%n",searchKey);
             }
-        }
+        // }
         
     }
 }
